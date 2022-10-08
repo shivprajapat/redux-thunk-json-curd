807eefdc922d6f08685967aff627b2b901a40134
shivprajapat <shivkumarprajapat232@gmail.com>
2022-10-08 17:23:47 +0530

    table add icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,16 @@ import {
   TableBody,
   Paper,
   Grid,
+  ButtonGroup,
+  IconButton,
+  Button,
 } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import { useDispatch, useSelector } from "react-redux";
 import { loadUsers } from "../redux/actions";
+import { Box } from "@mui/system";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -33,19 +39,22 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-
 const Home = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users.data);
   console.log("users :>> ", users);
   useEffect(() => {
     dispatch(loadUsers());
-    // eslint-disable-next-line 
+    // eslint-disable-next-line
   }, []);
   return (
     <div className="home">
       <Grid container spacing={2}>
         <Grid item xs={10} sx={{ margin: "auto" }}>
+          <Box sx={{ textAlign: "right" }} mb={2}>
+           
+              <Button variant="contained" color="secondary">Add User</Button>
+          </Box>
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 600 }} aria-label="customized table">
               <TableHead>
@@ -59,20 +68,34 @@ const Home = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {users.map((user, i) => {
-                  const { name, address, email, contact } = user;
-                  return (
-                    <StyledTableRow key={i}>
-                      <StyledTableCell component="th" scope="row">
-                        {i + 1}
-                      </StyledTableCell>
-                      <StyledTableCell>{name}</StyledTableCell>
-                      <StyledTableCell>{email}</StyledTableCell>
-                      <StyledTableCell>{address}</StyledTableCell>
-                      <StyledTableCell>{contact}</StyledTableCell>
-                    </StyledTableRow>
-                  );
-                })}
+                {users &&
+                  users.map((user, i) => {
+                    const { name, address, email, contact } = user;
+                    return (
+                      <StyledTableRow key={i}>
+                        <StyledTableCell component="th" scope="row">
+                          {i + 1}
+                        </StyledTableCell>
+                        <StyledTableCell>{name}</StyledTableCell>
+                        <StyledTableCell>{email}</StyledTableCell>
+                        <StyledTableCell>{address}</StyledTableCell>
+                        <StyledTableCell>{contact}</StyledTableCell>
+                        <StyledTableCell>
+                          <ButtonGroup
+                            variant="contained"
+                            aria-label="outlined primary button group"
+                          >
+                            <IconButton aria-label="delete" size="small">
+                              <EditIcon color="primary" />
+                            </IconButton>
+                            <IconButton aria-label="delete" size="small">
+                              <DeleteIcon color="error" />
+                            </IconButton>
+                          </ButtonGroup>
+                        </StyledTableCell>
+                      </StyledTableRow>
+                    );
+                  })}
               </TableBody>
             </Table>
           </TableContainer>
